88d165b51bc2d5071503ad2a713d27a84104216b
Suraj Mishra <mishrasur7@gmail.com>
2022-01-18 22:42:51 +0200

    Create leap_year.js

---
+++ leap_year.js
@@ -0,0 +1,17 @@
+//This isLeap function will check if the given year is leap year or not. 
+
+function isLeap(year) {   
+if(year % 4 === 0) {
+  if(year % 100 === 0) {
+      if(year % 400 === 0) {
+          return "Leap year."; 
+      } else {
+          return "Not leap year."; 
+      }     
+  } else {
+      return "Leap year."; 
+  }    
+} else {
+    return "Not leap year."; 
+}
+}
