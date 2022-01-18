bbab032440dbe9227e722cb97c0648ca868d1cfa
Suraj Mishra <mishrasur7@gmail.com>
2022-01-18 23:56:40 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,24 +1,22 @@
 //lets print numbers from 1 to hundred and if the number if divisible by 3 we print fizz, if the number if divisible by 5 we print buzz and 
 //if the number is divisible by both we pring fizzbuzz
 
-function fizzBuzz() {
-
 var nums = []; 
 
+function fizzBuzz() {
+  
 for(var i = 1; i <= 100; i++) {
   if(i % 3 === 0 && i % 5 === 0) {
-    nums.push["fizzbuzz"]; 
+    nums.push("fizzbuzz"); 
   } else if(i % 3 === 0) {
-      nums.push["fizz"]; 
+      nums.push("fizz"); 
   } else if(i % 5 === 0) {
-      nums.push["buzz"]; 
+      nums.push("buzz"); 
   } else {
-    nums.push[i]; 
+    nums.push(i); 
   }
   
-  return nums; 
-
-
 }
   
-  consloe.log(fizzBuzz()); 
+console.log(nums);   
+  
